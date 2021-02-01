3ef8fdfe149a27c4bed9e77b04856b7fb6c784ae
HUANG-Shiqing-hsq <78326044+HUANG-Shiqing-hsq@users.noreply.github.com>
2021-02-01 01:07:10 +0100

    Update VueOiseau.java

--- projet_HUANG/src/view/VueOiseau.java
+++ projet_HUANG/src/view/VueOiseau.java
@@ -1,3 +1,7 @@
+/** Class VueOiseau 
+    1. possède un attribut de type ArrayList<Oiseau>
+    2. une méthode dessiner(Graphics g) pour gérer l’affichage des oiseaux 
+    3. les 2 méthode: Ajoute_Oiseau et Supprime_Oiseau pour motifier Liste_Oiseau */
 package view;
 
 import model.Oiseau;
@@ -14,14 +18,17 @@ public class VueOiseau {
 
     }
 
+    /** Ajoute_Oiseau */
     public void Ajoute_Oiseau(Oiseau oiseau){
         this.Liste_Oiseau.add(oiseau);
     }
 
+    /** Supprime_Oiseau */
     public void Supprime_Oiseau(Oiseau oiseau){
         this.Liste_Oiseau.remove(Liste_Oiseau.indexOf(oiseau));
     }
 
+    /** dessiner */
     public void dessiner(Graphics g){
 
         for(Oiseau oiseau:Liste_Oiseau){
